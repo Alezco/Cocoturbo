1acd39f07474b607cb003d9aaf15b408aff660c6
Hadrien lamotte <hadrien.lamotte@gmail.com>
2017-06-30 11:04:33 +0200

    Add routes and layouts logic

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ListController@show');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/got', 'ListController@show')->middleware('auth');
