f527b9d964f062e661c2720bcfdc6c231963795b
Hadrien lamotte <hadrien.lamotte@gmail.com>
2017-07-04 20:47:13 +0200

    Add createRecette unit test

---
+++ tests/Unit/createRecetteTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class createRecetteTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testRecetteSansUrl()
+    {
+        $u1 = \App\User::create(array(
+            'name' => 'testRecette',
+            'email' => 'testRecette@coco.fr',
+            'password' => bcrypt('password'),
+        ));
+
+        $entree = \App\RecetteType::create(array(
+            'type_name' => 'entrée'
+        ));
+
+        $r1 = \App\Recette::create(array(
+            'recettes_name' => "Cake de test",
+            'description' => "1. 3 bouquets de persil plat.
+                              2. 1 bouquet de menthe fraîche.
+                              3. 2 oignons nouveaux.
+                              4. 2 à 3 tomates en grappe (les meilleures possible)
+                              5. 2 à 3 càs de boulgour (gros ou fin, il s'agit de blé concassé, en épicerie, grande surfaces ou magasin bio)
+                              6. le jus de 3 citrons jaunes.
+                              7. huile d'olive vierge extra et sel.",
+            'type_id' => $entree->id,
+        ));
+
+        $this->assertDatabaseHas('recettes', [
+            'recettes_name' => 'cake de test'
+        ]);
+    }
+
+    public function testRecetteAvecUrl()
+    {
+        $u1 = \App\User::create(array(
+            'name' => 'testRecetteAvecUrl',
+            'email' => 'testRecetteAvecUrl@coco.fr',
+            'password' => bcrypt('password'),
+        ));
+
+        $entree = \App\RecetteType::create(array(
+            'type_name' => 'entrée'
+        ));
+
+        $r1 = \App\Recette::create(array(
+            'recettes_name' => "Cake de test avec url",
+            'description' => "1. 3 bouquets de persil plat.
+                              2. 1 bouquet de menthe fraîche.
+                              3. 2 oignons nouveaux.
+                              4. 2 à 3 tomates en grappe (les meilleures possible)
+                              5. 2 à 3 càs de boulgour (gros ou fin, il s'agit de blé concassé, en épicerie, grande surfaces ou magasin bio)
+                              6. le jus de 3 citrons jaunes.
+                              7. huile d'olive vierge extra et sel.",
+            'image_url' => "http://www.ateliercuisinesenior.com/wp-content/uploads/2012/06/fotolia_244565422.jpg",
+            'type_id' => $entree->id,
+        ));
+
+        $this->assertDatabaseHas('recettes', [
+            'recettes_name' => 'Cake de test avec url'
+        ]);
+    }
+}
--- tests/Unit/createUserTest.php
+++ tests/Unit/createUserTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Webmozart\Assert\Assert;
 
 
 class createUserTest extends TestCase
@@ -24,11 +25,32 @@ class createUserTest extends TestCase
             'password' => bcrypt('password'),
         ));
 
-        $u1->push();
-
         $this->assertDatabaseHas('users', [
             'name' => 'coco',
             'email' => 'coco@coco.fr'
         ]);
+    }
+
+
+    public function testCreateUserSameEmail()
+    {
+        try {
+            $u1 = \App\User::create(array(
+                'name' => 'coco',
+                'email' => 'coco@coco.fr',
+                'password' => bcrypt('password'),
+            ));
+
+            $u2 = \App\User::create(array(
+                'name' => 'coco',
+                'email' => 'coco@coco.fr',
+                'password' => bcrypt('password'),
+            ));
+        }
+        catch(\Illuminate\Database\QueryException $ex){
+            $this->assertContains('coco@coco.fr', $ex->getMessage());
+            return;
+        }
+        $this->assertFalse(true);
     }
 }
